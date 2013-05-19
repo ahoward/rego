51f411938c38f8160203c58b5c8837fa02ef669a
ahoward <ara.t.howard@gmail.com>
2013-05-18 23:39:34 -0600

    cleanup.  back to fs-event.  v1.3.0

--- lib/rego.rb
+++ lib/rego.rb
@@ -3,7 +3,7 @@ require 'pathname'
 require 'yaml'
 
 module Rego
-  Version = '1.2.0' unless defined?(Version)
+  Version = '1.3.0' unless defined?(Version)
 
   def version
     Rego::Version
@@ -11,9 +11,8 @@ module Rego
 
   def dependencies
     {
-      'main'       => [ 'main'       , ' >= 4.8.1'   ] , 
-      #'rb-fsevent' => [ 'rb-fsevent' , ' >= 0.4.3.1' ] , 
-      'listen'     => [ 'listen'     , ' >= 1.0.2'   ] ,
+      'main'       => [ 'main'       , ' >= 4.8'   ] ,
+      'rb-fsevent' => [ 'rb-fsevent' , ' >= 0.9'   ] ,
     }
   end
 
@@ -35,6 +34,62 @@ module Rego
     libs = libs.join(' ').scan(/[^\s+]+/)
     Rego.libdir{ libs.each{|lib| Kernel.load(lib) } }
   end
+
+  def realpath(path)
+    Pathname.new(path.to_s).realpath.to_s
+  end
+
+  def say(phrase, *args)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    options[:color] = args.shift.to_s.to_sym unless args.empty?
+    keys = options.keys
+    keys.each{|key| options[key.to_s.to_sym] = options.delete(key)}
+
+    color = options[:color]
+    bold = options.has_key?(:bold)
+
+    parts = [phrase]
+
+    if STDOUT.tty?
+      parts.unshift(ANSI[color]) if color
+      parts.unshift(ANSI[:bold]) if bold
+      parts.push(ANSI[:clear]) if parts.size > 1
+    end
+
+    method = options[:method] || :puts
+
+    send(method, parts.join)
+  end
+
+  ANSI = {
+    :clear      => "\e[0m",
+    :reset      => "\e[0m",
+    :erase_line => "\e[K",
+    :erase_char => "\e[P",
+    :bold       => "\e[1m",
+    :dark       => "\e[2m",
+    :underline  => "\e[4m",
+    :underscore => "\e[4m",
+    :blink      => "\e[5m",
+    :reverse    => "\e[7m",
+    :concealed  => "\e[8m",
+    :black      => "\e[30m",
+    :red        => "\e[31m",
+    :green      => "\e[32m",
+    :yellow     => "\e[33m",
+    :blue       => "\e[34m",
+    :magenta    => "\e[35m",
+    :cyan       => "\e[36m",
+    :white      => "\e[37m",
+    :on_black   => "\e[40m",
+    :on_red     => "\e[41m",
+    :on_green   => "\e[42m",
+    :on_yellow  => "\e[43m",
+    :on_blue    => "\e[44m",
+    :on_magenta => "\e[45m",
+    :on_cyan    => "\e[46m",
+    :on_white   => "\e[47m"
+  }
 
   extend(Rego)
 end
